06f6a72d5f5b8317226541f0839fec58f30d1525
Bas de Ruijter <Bas@Funky-System.org>
2018-02-18 19:21:02 +0100

    windows and linux

--- src/libvm/os.c
+++ src/libvm/os.c
@@ -1,17 +1,17 @@
 #include "funkyvm/funkyvm.h"
 #include "os.h"
 
-#ifdef FUNKY_VM_OS_MACOS
-#include <mach-o/dyld.h>
-#include <limits.h>
-#include <string.h>
-
 static const char path_separator =
 #ifdef _WIN32
         '\\';
 #else
         '/';
 #endif
+
+#ifdef FUNKY_VM_OS_MACOS
+#include <mach-o/dyld.h>
+#include <limits.h>
+#include <string.h>
 
 char* get_executable_filepath() {
     char path[PATH_MAX];
@@ -24,14 +24,79 @@ char* get_executable_filepath() {
 #endif
 
 #ifdef FUNKY_VM_OS_WINDOWS
-char* get_executable_path() {
+#include <Windows.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+char* get_executable_filepath() {
+    DWORD last_error;
+    DWORD result;
+    DWORD path_size = 1024;
+    char* path      = malloc(1024);
+
+    for (;;)
+    {
+        memset(path, 0, path_size);
+        result     = GetModuleFileName(0, path, path_size - 1);
+        last_error = GetLastError();
+
+        if (0 == result)
+        {
+            free(path);
+            path = 0;
+            break;
+        }
+        else if (result == path_size - 1)
+        {
+            free(path);
+            /* May need to also check for ERROR_SUCCESS here if XP/2K */
+            if (ERROR_INSUFFICIENT_BUFFER != last_error)
+            {
+                path = 0;
+                break;
+            }
+            path_size = path_size * 2;
+            path = malloc(path_size);
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    if (!path)
+    {
+        fprintf(stderr, "Failure: %d\n", last_error);
+    }
+    else
+    {
+        printf("path=%s\n", path);
+    }
 
 }
 #endif
 
 #ifdef FUNKY_VM_OS_LINUX
-char* get_executable_path() {
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <stdio.h>
 
+char* get_executable_filepath() {
+    char path[PATH_MAX];
+    char dest[PATH_MAX];
+    memset(dest, 0, sizeof(dest)); // readlink does not null terminate!
+    struct stat info;
+    pid_t pid = getpid();
+    sprintf(path, "/proc/%d/exe", pid);
+    if (readlink(path, dest, PATH_MAX) == -1) {
+        perror("readlink");
+    } else {
+        return strdup(dest);
+    }
+    return NULL;
 }
 #endif
 
