f937eb4d52e953fd0f13b0a998736c80d17238be
Bas du Pré <me@basdupre.com>
2018-02-27 15:15:34 +0100

    return macro's

--- include/funkyvm/syscall.h
+++ include/funkyvm/syscall.h
@@ -9,6 +9,23 @@
 
 #define STACK_VALUE(STATE, i) ((vm_value_t *)(STATE->memory->main_memory + STATE->sp + i * sizeof(vm_value_t)))
 
+#define VM_RETURN_INT(STATE, VAL) { \
+    STATE->rr = (vm_value_t) { .type = VM_TYPE_INT, .int_value = VAL }; \
+    return; \
+}
+#define VM_RETURN_UINT(STATE, VAL) { \
+    STATE->rr = (vm_value_t) { .type = VM_TYPE_UINT, .uint_value = VAL }; \
+    return; \
+}
+#define VM_RETURN_FLOAT(STATE, VAL) { \
+    STATE->rr = (vm_value_t) { .type = VM_TYPE_FLOAT, .float_value = VAL }; \
+    return; \
+}
+#define VM_RETURN_STRING(STATE, VAL) { \
+    STATE->rr = (vm_value_t) { .type = VM_TYPE_STRING, .pointer_value = vm_create_string(STATE, VAL) }; \
+    return; \
+}
+
 typedef void (*vm_syscall_t)(CPU_State *state);
 
 typedef __attribute__((aligned(1))) struct vm_syscall_table_t {
